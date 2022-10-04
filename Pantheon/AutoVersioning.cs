497f1995c8392419435495e02a51a4a59c1caf6e
Gert Dunon <gert.dunon@creonis.be>
2022-10-04 10:40:43 +0200

    changed texttemplate path

--- Pantheon/AutoVersioning.cs
+++ Pantheon/AutoVersioning.cs
@@ -1,6 +1,6 @@
 ﻿
 using System.Reflection;
 
-[assembly: AssemblyVersion("2022.10.04.0800")]
-[assembly: AssemblyFileVersion("2022.10.04.0800")]
+[assembly: AssemblyVersion("2022.10.04.0840")]
+[assembly: AssemblyFileVersion("2022.10.04.0840")]
 
